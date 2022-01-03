d8c356143d4f2132a5aaa02333370e8cfdf3dd79
cleisommais <cleisommais@gmail.com>
2022-01-03 20:51:08 +0000

    Added the new components

--- front-end/app-portal-apigee/src/app/app.module.ts
+++ front-end/app-portal-apigee/src/app/app.module.ts
@@ -4,10 +4,16 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { HomeComponent } from './home/home.component';
+import { HeaderComponent } from './header/header.component';
+import { BottomComponent } from './bottom/bottom.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    HeaderComponent,
+    BottomComponent
   ],
   imports: [
     BrowserModule,
